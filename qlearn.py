dca43344d290c7e33a8c60614062da543e2ada26
mjv64 <38958070+mjv64@users.noreply.github.com>
2023-05-28 01:23:45 -0400

    Code for 25 million games each QAI

--- qlearn.py
+++ qlearn.py
@@ -8,7 +8,33 @@ def main():
     QAI3 = QLearningAI(.2)
     QAIW = 0
     RandomW = 0
-    for i in range(10000000):
+    for i in range(25000000):
+        engine1 = GameEngine()
+        engine1.addPlayer(QAI1)
+        randomAI = RandomAI()
+        engine1.addPlayer(randomAI)
+        winner = engine1.runGame()
+        if winner == QAI1:
+            QAIW += 1
+        else:
+            RandomW += 1
+    print("QAI1 won " + str(QAIW) + " games vs Random won " + str(RandomW) + " games.")
+    QAIW = 0
+    RandomW = 0
+    for i in range(25000000):
+        engine1 = GameEngine()
+        engine1.addPlayer(QAI2)
+        randomAI = RandomAI()
+        engine1.addPlayer(randomAI)
+        winner = engine1.runGame()
+        if winner == QAI2:
+            QAIW += 1
+        else:
+            RandomW += 1
+    print("QAI2 won " + str(QAIW) + " games vs Random won " + str(RandomW) + " games.")
+    QAIW = 0
+    RandomW = 0
+    for i in range(25000000):
         engine1 = GameEngine()
         engine1.addPlayer(QAI3)
         randomAI = RandomAI()
@@ -18,7 +44,7 @@ def main():
             QAIW += 1
         else:
             RandomW += 1
-    print("QAI won " + str(QAIW) + " games vs Random won " + str(RandomW) + " games.")
+    print("QAI3 won " + str(QAIW) + " games vs Random won " + str(RandomW) + " games.")
 
 if __name__ == '__main__':
     main()
